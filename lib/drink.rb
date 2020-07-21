667ffefebeb18593a85a4fbafb56b0f394d9ee9c
edmundopierre@yahoo.com <edmundo@LAPTOP-LTB84QPJ.localdomain>
2020-07-21 11:00:40 -0700

    Add two classes methode

--- lib/drink.rb
+++ lib/drink.rb
@@ -1,11 +1,26 @@
 class Drink
-attr_accessor :drink_name, :drink_id, :link_picture,:ingredient, :instructions ,:category,:changed_date, :glass,:measure
+        attr_accessor :drink_name, :drink_id, :link_picture,:ingredient, :instructions ,:category,:changed_date, :glass,:measure
 
-@@all =[]
+        @@all =[]
 
-def initialize(drink_name:,link_picture: ,drink_id:, ingredient:)
-    @drink_name = drink_name
-    @drink_id = drink_id
+    def initialize(drink_name:,link_picture: ,drink_id:, ingredient:)
+        @drink_name = drink_name
+        @drink_id = drink_id
+        @ingredient = ingredient
+        @link_picture = link_picture
+        @@all << self
 
-end
+    end
+
+    def self.all
+
+        @@all
+ 
+    end
+
+    def self.find_by_ingredient(ingredient)
+        @@all.select {|value| value.ingredient == ingredient}
+
+    end
+
 end
