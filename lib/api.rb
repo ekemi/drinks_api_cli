bba95062ce5505b766b5d889b66d4a3f9adbbefc
edmundopierre@yahoo.com <edmundo@LAPTOP-LTB84QPJ.localdomain>
2020-07-21 09:38:52 -0700

    made an instance methode get_drinks

--- lib/api.rb
+++ lib/api.rb
@@ -1,5 +1,13 @@
 class Api
 
+    def self.get_drinks(ingredient)
 
+        url = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=#{ingredient}"
+        response = Net::HTTP.get(URI(url))
+        drinks = JSON.parse(response)['drinks']# Allowed us to get an array.
+        # Make a drink Object.
+        drinks.each do |drink_ob| 
+          Drink.new(drink_name: drink_ob["strDrink"],link_picture: drink_ob["strDrinkThumb"],drink_id: drink_ob["idDrink"],ingredient:ingredient)
+      end
 
 end
