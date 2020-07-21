882d2bd49ee5e8509d2116ce7bc7b830e12ea94d
edmundopierre@yahoo.com <edmundo@LAPTOP-LTB84QPJ.localdomain>
2020-07-21 10:44:40 -0700

    create an initialize method with arguments

--- lib/drink.rb
+++ lib/drink.rb
@@ -2,4 +2,8 @@ class Drink
 attr_accessor :drink_name, :drink_id, :link_picture,:ingredient, :instructions ,:category,:changed_date, :glass,:measure
 
 @@all =[]
+
+def initialize(drink_name:,link_picture: ,drink_id:, ingredient:)
+
+end
 end
