42ea7b4182c4ca19ff4536182ffa49447466617f
edmundopierre@yahoo.com <edmundo@LAPTOP-LTB84QPJ.localdomain>
2020-07-21 10:45:51 -0700

    create an initialize instance variable

--- lib/drink.rb
+++ lib/drink.rb
@@ -4,6 +4,7 @@ attr_accessor :drink_name, :drink_id, :link_picture,:ingredient, :instructions ,
 @@all =[]
 
 def initialize(drink_name:,link_picture: ,drink_id:, ingredient:)
+    @drink_name = drink_name
 
 end
 end
