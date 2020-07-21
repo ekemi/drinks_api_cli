5532a3ebfdf1338923cee3ed50d24d57c58f5e0b
edmundopierre@yahoo.com <edmundo@LAPTOP-LTB84QPJ.localdomain>
2020-07-21 10:02:44 -0700

    made an class method get_drink_details

--- lib/api.rb
+++ lib/api.rb
@@ -9,5 +9,17 @@ class Api
         drinks.each do |drink_ob| 
           Drink.new(drink_name: drink_ob["strDrink"],link_picture: drink_ob["strDrinkThumb"],drink_id: drink_ob["idDrink"],ingredient:ingredient)
       end
+      
+    def self.get_drink_details(drink) 
+        url = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=#{drink.drink_id}"
+        response = Net::HTTP.get(URI(url))
+        data_details = JSON.parse(response)["drinks"]
+        drink.instructions = data_details[0]["strInstructions"]
+        drink.glass = data_details[0]["strGlass"]
+        drink.glass = data_details[0]["strGlass"]
+        drink.measure = data_details[0]["strMeasure1"]
+        drink.category = data_details[0]["strCategory"]
+        drink.changed_date = data_details[0]["dateModified"]
+     end
 
 end
